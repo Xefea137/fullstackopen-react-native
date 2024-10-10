22c8d07291aea32844d3f27103b3cd8d02a2ddac
Moe Khan <mobashir16@gmail.com>
2024-10-10 17:10:30 +0530

    10.12 .env

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -3,10 +3,12 @@ import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
 import { ApolloProvider } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.expoConfig);
   return (
     <>
       <NativeRouter>
--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -17,28 +17,3 @@ const useRepositories = () => {
 };
 
 export default useRepositories;
-
-/*import { useState, useEffect } from 'react';
-
-const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
-
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    const response = await fetch('http://192.168.0.103:5000/api/repositories');
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
-};
-
-export default useRepositories;*/
