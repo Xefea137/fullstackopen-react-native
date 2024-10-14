cb52e8417c62b16e4b553fb9cb4e4c93b8cbcf7f
Moe Khan <mobashir16@gmail.com>
2024-10-14 09:30:28 +0530

    10.15 - 10.46 access token and signout

--- rate-repository-app/src/graphql/queries.js
+++ rate-repository-app/src/graphql/queries.js
@@ -1,21 +1,30 @@
 import { gql } from '@apollo/client';
 
 export const ALL_REPOSITORIES = gql`
-query Repositories {
-  repositories {
-    edges {
-      node {
-        id
-        ownerAvatarUrl
-        fullName
-        description
-        language
-        stargazersCount
-        forksCount
-        reviewCount
-        ratingAverage
+  query Repositories {
+    repositories {
+      edges {
+        node {
+          id
+          ownerAvatarUrl
+          fullName
+          description
+          language
+          stargazersCount
+          forksCount
+          reviewCount
+          ratingAverage
+        }
       }
     }
   }
-}
+`
+
+export const ME_DATA = gql`
+  query Me {
+    me {
+      id
+      username
+    }
+  }
 `
